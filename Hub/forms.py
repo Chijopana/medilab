ce000fd50118f3cd00f090ed05421e5cc17139ec
Jose Blondel <164395005+Chijopana@users.noreply.github.com>
2026-08-29 15:07:46 +0200

    Actualizar README, limpiar assets no usados, excluir venv del repo

--- Hub/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from django.contrib.auth.models import User
-from Perfiles.models import *
-
-class UserForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ['username', 'password']
-
