00b9c59432842eeb7d4eec35394974aed8519b61
Jose Blondel <164395005+Chijopana@users.noreply.github.com>
2026-08-29 15:17:50 +0200

    Revert "Actualizar README, limpiar assets no usados, excluir venv del repo"
    This reverts commit e1ed4d8fc5803bce762d1072d99388f7d551073c.

--- E404/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('', views.error_404, name='error_404'),
-]
--- E404/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def error_404(request):
-    return render(request,'error_404.html')
